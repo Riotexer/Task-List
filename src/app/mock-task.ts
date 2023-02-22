ebebd75aec67ba25d9bdb640e3f68ba203a3c62b
Riotexer <francofjlh@gmail.com>
2023-02-22 10:46:17 -0300

    Agrega mock de tareas y la interfaz de las mismas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASK: Task[] = [
+    {
+        id: 1,
+        text: 'Terminar modulo de Angular',
+        day: 'Febrero 27',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Comprar comida para fin de semana',
+        day: 'Febrero 25',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Partido futbol 5',
+        day: 'Febrero 26',
+        reminder: true
+    },
+    {
+        id: 4,
+        text: 'Banner Genshin',
+        day: 'Febrero 28',
+        reminder: true
+    }
+]
