b6c2070f8e288d975ea5a395fe70a7cc72e3317b
Riotexer <francofjlh@gmail.com>
2023-02-26 20:48:14 -0300

    Agrega boton para ocultar/mostrar formulario

--- src/app/component/add-task/add-task.component.ts
+++ src/app/component/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 import { Task } from 'src/app/Task';
 
 @Component({
@@ -8,9 +10,18 @@ import { Task } from 'src/app/Task';
 })
 export class AddTaskComponent {
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  
   text: string = '';
   day: string = '';
   reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription
+
+  constructor(
+    private uiService : UiService
+  ){
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
 
   onSubmit() {
     if(this.text.length === 0){
