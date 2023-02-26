94b7e2bc493c6f29969cbac948431a6eff5dff1a
Riotexer <francofjlh@gmail.com>
2023-02-25 21:05:07 -0300

    Agrega formulario para guardar tareas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
+import { Form, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { ButtonComponent } from './component/button/button.component';
 import { TasksComponent } from './component/tasks/tasks.component';
 import { TasksItemComponent } from './component/tasks-item/tasks-item.component';
+import { AddTaskComponent } from './component/add-task/add-task.component';
 
 @NgModule({
   declarations: [
@@ -14,12 +16,14 @@ import { TasksItemComponent } from './component/tasks-item/tasks-item.component'
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TasksItemComponent
+    TasksItemComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
