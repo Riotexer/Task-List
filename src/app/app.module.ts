4ff5b24e62446ed8882eebd91d1e8d6e0484c143
Riotexer <francofjlh@gmail.com>
2023-02-26 21:03:08 -0300

    Agrega una seccion About

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
-import { Form, FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { ButtonComponent } from './component/button/button.component';
 import { TasksComponent } from './component/tasks/tasks.component';
 import { TasksItemComponent } from './component/tasks-item/tasks-item.component';
 import { AddTaskComponent } from './component/add-task/add-task.component';
+import { AboutComponent } from './component/about/about.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component:TasksComponent },
+  { path: 'about', component:AboutComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,13 +25,15 @@ import { AddTaskComponent } from './component/add-task/add-task.component';
     ButtonComponent,
     TasksComponent,
     TasksItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
